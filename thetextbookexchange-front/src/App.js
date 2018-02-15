29d142aa1f2ca45656b831746c25d114cb14b0b6
zeman95 <zachgarvis@yahoo.com>
2018-02-15 12:19:57 -0600

    .gitignore fix

--- thetextbookexchange-front/src/App.js
+++ thetextbookexchange-front/src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import React, { Component } from 'react';
+//import {Router, Route} from 'react-router';
+import { render } from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
 
-export default App;
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>this is a rather boring page</p>
+  </div>
+)
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+    </div>
+  </Router>
+)
+export default BasicExample
--- thetextbookexchange-front/src/index.js
+++ thetextbookexchange-front/src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+/*
+import React, { Component } from 'react';
+import {Router, Route} from 'react-router';
+import { render } from 'react-dom';*/
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
