3e5096771f45124a9a78e793ea8b862836b97760
Austin Nielsen <austinpnielsen@hotmail.com>
2018-02-15 16:24:45 -0600

    Added Sidebar

--- thetextbookexchange-front/src/App.js
+++ thetextbookexchange-front/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './SideBarCSS.css';
 //import React, { Component } from 'react';
 //import {Router, Route} from 'react-router';
 import { render } from 'react-dom';
@@ -16,6 +17,24 @@ const Home = () => (
   </div>
 )
 
+const Account = () => (
+  <div>
+    <h2>Account</h2>
+  </div>
+)
+
+const MyBooks = () => (
+  <div>
+    <h2>My Books</h2>
+  </div>
+)
+
+const Settings = () => (
+  <div>
+    <h2>Settings</h2>
+  </div>
+)
+
 const About = () => (
   <div>
     <h2>About</h2>
@@ -26,15 +45,20 @@ const About = () => (
 const BasicExample = () => (
   <Router>
     <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-
-      <hr/>
-
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
+      <div id="mySidenav" class="sidenav">
+        <a href="/">Home</a>
+        <a href="/account">Account</a>
+        <a href="/mybooks">My Books</a>
+        <a href="/settings">Settings</a>
+        <a href="/about">About</a>
+      </div>
+      <div class="mainBody App-header">
+        <Route exact path="/" component={Home}/>
+        <Route path="/account" component={Account}/>
+        <Route path="/mybooks" component={MyBooks}/>
+        <Route path="/settings" component={Settings}/>
+        <Route path="/about" component={About}/>
+      </div>
     </div>
   </Router>
 )
