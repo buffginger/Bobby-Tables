5b99f0fb95ff7b812d014f68527a8aa5193401fb
Matthew C. Tran <matthewtran@unomaha.edu>
2018-02-16 01:29:07 -0600

    T1: Easter Egg.

--- thetextbookexchange-front/src/Components/NotFound/notfound.js
+++ thetextbookexchange-front/src/Components/NotFound/notfound.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Tumbeast from '../../Assets/Images/404.png';
 
 // Homepage component/module.
 class NotFound extends React.Component {
@@ -13,7 +14,8 @@ class NotFound extends React.Component {
                     <div className="jumbotron jumbotron-fluid">
                         <div className="container">
                             <h1 className="display-1">404 Not Found</h1>
-                            <p className="lead">Uh Oh! Looks like we couldn't find a page at that URL.</p>
+                            <img className="img-fluid" style={{"max-height" : "480px"}} src={Tumbeast} alt="404 Tumbeast"/>
+                            <p className="lead">Uh Oh! Looks like we couldn't find a page at that URL...</p>
                             <button onClick={this.context.router.history.goBack} type="button"
                                     className="btn btn-outline-primary btn-lg">Go Back<br/><i
                                 className="material-icons">arrow_back</i></button>
