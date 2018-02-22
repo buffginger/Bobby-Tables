84f63975b9c253131d063babd603a7519096fbb1
zeman95 <zachgarvis@yahoo.com>
2018-02-21 22:30:07 -0600

    made custom homepage

--- thetextbookexchange-front/src/Components/Home/home.js
+++ thetextbookexchange-front/src/Components/Home/home.js
@@ -13,10 +13,51 @@ class Home extends React.Component {
                         <div className="jumbotron jumbotron-fluid">
                             <div className="container">
                                 <h1 className="display-1">Homepage</h1>
-                                <p className="lead">This is the new layout for our pages. Please follow project
-                                    structure
-                                    conventions. If you have any questions let me know. Edit me in
-                                    src/Components/Home/home.js</p>
+                                <div>
+                                    <form action="/searchResults.js">
+                                        <div class="input-group mb-3">
+                                            <input type="text" class="form-control" id="databaseQuery" placeholder="Search - ISBN, Author, Publisher & More!"/>
+                                            
+                                            <div class="input-group-append">
+                                                <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Select Search Type</button>
+                                                <div class="dropdown-menu">
+                                                    <a selected class="dropdown-item" href="#">All</a>
+                                                    <a class="dropdown-item" href="#">Author</a>
+                                                    <a class="dropdown-item" href="#">ISBN-10</a>
+                                                    <a class="dropdown-item" href="#">ISBN-13</a>
+                                                    <a class="dropdown-item" href="#">Title</a>
+                                                    <a class="dropdown-item" href="#">Publisher</a>
+                                                </div>
+                                            </div>
+                                            <div class="input-group-append">
+                                                <span class="input-group-text"><button type="submit" class="btn btn-primary">Submit</button></span>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <p className="lead">
+                                    <hr/>
+                                    
+                                    <br/>
+                                    <div class="card">
+                                        <div class="card-header"><b>Post</b> #1213411 | <b>Date Posted:</b> 02/21/2018 | <b>Price Negotiable:</b> No</div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Tumbeasts - Special Edition | $5</h5>
+                                            <p class="card-text" maxlength="10">
+                                                    <div class="limitText">
+                                                    Hello All! I am selling the tumbeasts book - as the title specifies. The tumbeasts within vary in emotions and mental stability. They are collected from all over the internet and therefore are very unique and also compounded into a special edition, just for you! The book is 5$. What a bargain.
+                                                    </div>
+                                            </p>
+                                            <a href="./samplebook" class="btn btn-primary">Check it out!</a>
+                                        </div>
+                                    </div> 
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                </p>
                             </div>
                         </div>
                     </div>
