7f9c3025ad324f0f21b05d7ea256253f357346c6
Matthew C. Tran <matthewtran@unomaha.edu>
2018-02-25 18:51:05 -0600

    T6: Sell books page.

---
+++ thetextbookexchange-front/src/Components/SellBook/sellbook.js
@@ -0,0 +1,67 @@
+import React from 'react'
+// Page has Sidebar
+import Sidebar from '../Sidebar/sidebar';
+
+// Homepage component/module.
+class SellBook extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="container-fluid sidenavWrapper p-0">
+                    <Sidebar/>
+                    <div className="maincontent-container-fullwidth">
+                        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                            <span class="navbar-brand mb-0 h1">Sell a Book</span>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav mr-auto">
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Actions
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <div class="dropdown-divider"></div>
+                                            <a class="dropdown-item" href="#">Something else here</a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                        <div class="card">
+                            <h5 class="card-header">Book Information</h5>
+                            <div class="card-body">
+                                <h5 class="card-title">Special title treatment</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <h5 class="card-header">Pricing</h5>
+                            <div class="card-body">
+                                <h5 class="card-title">Special title treatment</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <h5 class="card-header">Location</h5>
+                            <div class="card-body">
+                                <h5 class="card-title">Special title treatment</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+// Export this module to be imported from App.js.
+export default SellBook
