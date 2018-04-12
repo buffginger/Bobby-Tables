fb066ea3f8b397ec8882f3260d193f1afc387b4c
zeman95 <zachgarvis@yahoo.com>
2018-04-11 20:47:08 -0500

    got account page to display information stored in the authentication cookie

--- thetextbookexchange-front/src/Components/Account/account.js
+++ thetextbookexchange-front/src/Components/Account/account.js
@@ -7,12 +7,6 @@ import { Link } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 
 
-// Homepage component/module.
-//class Home extends React.Component {
-// additional classes go here
-
-
-
 class Home extends React.Component {
     
     constructor(props) {
@@ -20,9 +14,105 @@ class Home extends React.Component {
         this.state = {nameRet: '',
                       emailRet: ''
                     };
-       
+
                     const cookies = new Cookies();
-                    var temp;
+                    
+                   //var json;
+                   //var nameRet;
+                   //var emailRet;
+                   
+           
+                   axios.get('http://localhost:8000/api/userInfo', {
+                       headers: {
+                           Authorization: 'Bearer ' + cookies.get('TBEAuthToken'),
+                       }
+                   })
+                   .then(res => {
+                        
+                       //json = JSON.parse(res.data);
+                       console.log(res.data)
+                       //nameRet = res.data.name;
+                       //emailRet  = res.data.email;
+                       //alert('Name:' + nameRet);
+                       
+                       
+                       this.setState({nameRet: res.data.name});
+                       this.setState({emailRet: res.data.email});
+                       //alert('Name using setstate:' + this.state.nameRet);
+   
+                       
+                       
+                   })
+                   .catch(function (error) {
+                   console.log(error);
+                   alert('error:' + error);
+                   });
+      }
+
+    render() {
+        return (
+            <div>
+                <div className="container-fluid sidenavWrapper p-0">
+                    <Sidebar/>
+                    <div className="maincontent-container-fullwidth">
+                        <div className="jumbotron jumbotron-fluid jumbotron-minimalpadding">
+                            <div className="container-minimal">
+                            
+                            <h1 className="display-1" style={{"paddingLeft": 15}}>Account Information</h1>
+                            <p className="lead" style={{"paddingLeft": 25}}> <b>Welcome back <Header headerProp = {this.state.nameRet}/>!</b></p> 
+                                <hr/> 
+
+                                
+                                <div className="container-minimal">
+                                        <div className="card" >
+                                            <div className="card-body">
+                                                
+                                               
+                                                        <b>User Name:</b>   <Header headerProp = {this.state.nameRet}/>  <br/>
+                                                        <b>Current Email:</b> <PrintEmail printEmailProp = {this.state.emailRet}/> <br/>
+                                                        <br/>
+                                                        <a  href="/user_settings"><i className="fa fa-gear"></i><b>Update Information</b></a>
+                                                
+                                            </div>
+                                            <br/>
+                                        </div>
+                            </div>                                                   
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+class Header extends React.Component {
+    render() {
+       return (
+          <i>{this.props.headerProp}</i>
+         
+       );
+    }
+ }
+ class PrintEmail extends React.Component {
+    render() {
+       return (
+          <i>{this.props.printEmailProp}</i>
+         
+       );
+    }
+ }
+
+
+
+
+// Export this module to be imported from App.js.
+export default Home
+
+
+/*const cookies = new Cookies();
+                     var temp;
                     //var json;
                     //var nameRet;
                     //var emailRet;
@@ -55,11 +145,12 @@ class Home extends React.Component {
                     });
         
         //this.getUserInfo = this.getUserInfo.bind(this); // this right now just shows what was entered
-        //componentDidMount;
+        //componentDidMount;*/
+
+
         
-      }
     
-      getUserInfo(event) {
+      /*getUserInfo(event) {
                 const cookies = new Cookies();
                 //var json;
                 //var nameRet;
@@ -91,7 +182,7 @@ class Home extends React.Component {
                 });
             
                 event.preventDefault();
-            }
+            }*/
           
             
 
@@ -161,51 +252,3 @@ class Home extends React.Component {
                 });
                 
             }*/
-            
-            
-    
-
-    render() {
-        return (
-            <div>
-                <div className="container-fluid sidenavWrapper p-0">
-                    <Sidebar/>
-                    <div className="maincontent-container-fullwidth">
-                        <div className="jumbotron jumbotron-fluid jumbotron-minimalpadding">
-                            <div className="container-minimal">
-                            
-                            <h1 className="display-1" style={{"paddingLeft": 15}}>Account Information</h1>
-                            <p className="lead" style={{"paddingLeft": 25}}> <b>Welcome back <i>(user name)</i>!</b></p> 
-                                <hr/> 
-
-                                
-                                <div className="container-minimal">
-                                        <div className="card" >
-                                            <div className="card-body">
-                                                
-                                               
-                                                        <b>User Name:</b>   (database entry)  <br/>
-                                                        <b>Current Email:</b> (database entry) <br/>
-                                                        <b>Contact Number:</b> (database entry) <br/>
-                                                        <br/>
-                                                        <a  href="/user_settings"><i className="fa fa-gear"></i><b>Update Information</b></a>
-                                                
-                                            </div>
-                                            <br/>
-                                        </div>
-                            </div>                       
-
-
-
-                            
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
-// Export this module to be imported from App.js.
-export default Home
