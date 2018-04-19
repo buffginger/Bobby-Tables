dbc8462578b3f57de45b0c509d7578adc5240cdd
Matthew C. Tran <matthewtran@unomaha.edu>
2018-04-19 03:12:21 -0500

    Final build for demo presentation (revised).

--- thetextbookexchange-front/src/Components/ViewBook/viewbook.js
+++ thetextbookexchange-front/src/Components/ViewBook/viewbook.js
@@ -17,11 +17,22 @@ class ViewBook extends React.Component {
 
         const { handle } = this.props.match.params
         try {
-            const response = axios.get(`https://api.thetextbookexchange.club/api/books/${handle}`)
+            const cookies = new Cookies();
+            const response = axios.get(`https://api.thetextbookexchange.club/api/books/${handle}`, {
+                headers: {
+                    Authorization: 'Bearer ' + cookies.get('TBEAuthToken'),
+                },
+            })
         } catch (error) {
             this.props.history.push("/home");
         }
-        axios.get(`https://api.thetextbookexchange.club/api/books/${handle}`)
+
+        const cookies = new Cookies();
+        axios.get(`https://api.thetextbookexchange.club/api/books/${handle}`, {
+            headers: {
+                Authorization: 'Bearer ' + cookies.get('TBEAuthToken'),
+            },
+        })
         .then(res => res.data)
         .then(book => {
             this.setState({
